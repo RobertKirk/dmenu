3458ca1df3731b1d0a7cd62332a619c8f712f022
Robert Kirk <robert.kirk.3.14@gmail.com>
2019-06-16 23:05:40 +0100

    addinf dmenu_run_favs for running favourites, and fixing colours to solarized light

--- config.h
+++ config.h
@@ -12,12 +12,22 @@ static const char col_gray2[]       = "#eee8d5";
 static const char col_gray3[]       = "#839496";
 static const char col_gray4[]       = "#eeeeee";
 static const char col_cyan[]        = "#005577";
+
+static const char s_base03[]        = "#002b36";
+static const char s_base02[]        = "#073642";
+static const char s_base01[]        = "#586e75";
+static const char s_base00[]        = "#657b83";
+static const char s_base0[]         = "#839496";
+static const char s_base1[]         = "#93a1a1";
+static const char s_base2[]         = "#eee8d5";
+static const char s_base3[]         = "#fdf6e3";
+
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
 	[SchemeOut] =  { "#000000", "#00ffff" },
-	[SchemeNorm] = { col_gray3, col_gray1 },
-	[SchemeSel]  = { col_gray1, col_gray3 },
+	[SchemeNorm] = { s_base00, s_base3 },     /* SchemeNorm light */
+	[SchemeSel]  = { s_base00, s_base2 },      /* SchemeSel light */
 };
 static unsigned int maxhist    = 15;
 static int histnodup           = 1;	/* if 0, record repeated histories */
